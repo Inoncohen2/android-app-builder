7424aa571667dd4574e75aed2e18f47b3e9c2a21
Inon Cohen <96375193+Inoncohen2@users.noreply.github.com>
2026-02-11 11:18:47 +0200

    Create settings_screen.dart

---
+++ lib/settings_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'app_config.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.info_outline),
+            title: Text('About ${AppConfig.appName}'),
+            subtitle: Text('Version 1.0.0'),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.privacy_tip_outlined),
+            title: Text('Privacy Policy'),
+            trailing: Icon(Icons.open_in_new),
+            onTap: () => _launchURL(AppConfig.privacyPolicyUrl),
+          ),
+          if (AppConfig.termsUrl.isNotEmpty)
+            ListTile(
+              leading: Icon(Icons.description_outlined),
+              title: Text('Terms of Service'),
+              trailing: Icon(Icons.open_in_new),
+              onTap: () => _launchURL(AppConfig.termsUrl),
+            ),
+        ],
+      ),
+    );
+  }
+  
+  Future<void> _launchURL(String url) async {
+    final uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalBrowser);
+    }
+  }
+}
